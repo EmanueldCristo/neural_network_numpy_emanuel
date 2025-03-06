02d0d6214cee313adc8304d148d6438ed092afbf
EmanueldCristo <emanueldecristo065@gmail.com>
2025-03-06 08:44:13 -0600

    Commit 1

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.neural_numpy import train_model
+
+# Punto de entrada del script
+if __name__ == "__main__":
+    """
+    Ejecuta la función principal de entrenamiento del modelo cuando el script se ejecuta directamente.
+    """
+    train_model()
